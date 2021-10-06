9e21b2150750dcb4ace1cef5c0d0506651a6d8c3
denliedesign <customdenlie@gmail.com>
2021-10-05 21:07:38 -0500

    new testimonials

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -10,9 +10,73 @@
                        <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="1" aria-label="Slide 2"></button>
                        <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="2" aria-label="Slide 3"></button>
                        <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                       <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                       <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                       <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                       <button type="button" data-bs-target="#carouselTestimonial" data-bs-slide-to="7" aria-label="Slide 8"></button>
                    </div>
                    <div class="carousel-inner">
                        <div class="carousel-item active">
+                           <div class="px-4 py-3">
+                               <p class="fw-bold d-none d-md-block mb-0" style="font-size: 1.2em;">
+                                   Thank you for all that you have done this year to make Ellie’s first year incredible.  Having her former studio close during the pandemic, although unfortunate, led her to BGDC and I think it was a blessing.   Besides the dancing, which is incredible, the emphasis on team building and sisterhood is so important.  Kids need this more than ever.   BGDC is truly amazing, like no other studio.  Ellie has had the chance to grow and thrive.
+                               </p>
+                               <p class="fw-bold d-block d-md-none mb-0" style="font-size: 0.7em;">
+                                   Thank you for all that you have done this year to make Ellie’s first year incredible.  Having her former studio close during the pandemic, although unfortunate, led her to BGDC and I think it was a blessing.   Besides the dancing, which is incredible, the emphasis on team building and sisterhood is so important.  Kids need this more than ever.   BGDC is truly amazing, like no other studio.  Ellie has had the chance to grow and thrive.
+                               </p>
+                               <div class="d-flex align-items-center">
+                                   {{--                                   <ion-icon name="person-circle-outline" style="font-size: 2em; color: #d33;"></ion-icon>--}}
+                                   &nbsp; <span class="lead d-none d-md-block" style="font-size: 1.2em;">-Erin Baker</span>
+                                   <span class="lead d-block d-md-none" style="font-size: 0.7em;">-Erin Baker</span>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="carousel-item">
+                           <div class="px-4 py-3">
+                               <p class="fw-bold d-none d-md-block mb-0" style="font-size: 1.2em;">
+                                   The 2020 recital was truly perfect! The shows were beautifully run, well organized, fun, and COVID safe.  We loved the special touches like the gorgeous balloon displays, photo backdrops, and signs recognizing 5/10 year students and graduating seniors.  We noticed every detail and loved it all!  Thanks again for making BGDC a great dance home for our girls.
+                               </p>
+                               <p class="fw-bold d-block d-md-none mb-0" style="font-size: 0.7em;">
+                                   The 2020 recital was truly perfect! The shows were beautifully run, well organized, fun, and COVID safe.  We loved the special touches like the gorgeous balloon displays, photo backdrops, and signs recognizing 5/10 year students and graduating seniors.  We noticed every detail and loved it all!  Thanks again for making BGDC a great dance home for our girls.
+                               </p>
+                               <div class="d-flex align-items-center">
+                                   {{--                                   <ion-icon name="person-circle-outline" style="font-size: 2em; color: #d33;"></ion-icon>--}}
+                                   &nbsp; <span class="lead d-none d-md-block" style="font-size: 1.2em;">-Rebecca Shaw</span>
+                                   <span class="lead d-block d-md-none" style="font-size: 0.7em;">-Rebecca Shaw</span>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="carousel-item">
+                           <div class="px-4 py-3">
+                               <p class="fw-bold d-none d-md-block mb-0" style="font-size: 1.2em;">
+                                   Our daughter Sophia had an amazing experience at her recital! This year has taught her to come out of her shell a bit more and really boost her confidence with the help of Breaking Ground! Hats off to Marissa and the entire staff for doing such an amazing job of keeping our kids safe and smiling the whole year!
+                               </p>
+                               <p class="fw-bold d-block d-md-none mb-0" style="font-size: 0.7em;">
+                                   Our daughter Sophia had an amazing experience at her recital! This year has taught her to come out of her shell a bit more and really boost her confidence with the help of Breaking Ground! Hats off to Marissa and the entire staff for doing such an amazing job of keeping our kids safe and smiling the whole year!
+                               </p>
+                               <div class="d-flex align-items-center">
+                                   {{--                                   <ion-icon name="person-circle-outline" style="font-size: 2em; color: #d33;"></ion-icon>--}}
+                                   &nbsp; <span class="lead d-none d-md-block" style="font-size: 1.2em;">-Valerie Racanelli</span>
+                                   <span class="lead d-block d-md-none" style="font-size: 0.7em;">-Valerie Racanelli</span>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="carousel-item">
+                           <div class="px-4 py-3">
+                               <p class="fw-bold d-none d-md-block mb-0" style="font-size: 1.1em;">
+                                   My daughter Ella, a graduating senior and Senior Company member, ended her competition season on a perfect note with her best friend Lauren at her side.  The girls took one last picture together on stage and looking back, it really depicts the strong friendships that my daughter has gained through BG.  It was great that Ella and Lauren ended their time at BG together.  They still talk about how their friendship began with they were in Petite Ensemble.  Thank you to Marissa and her team for all they have done for Ella.  When she was 9, I remember asking her what she loves the most about dance and she said it made her feel free and that she can be herself.  I will always remember that!
+                               </p>
+                               <p class="fw-bold d-block d-md-none mb-0" style="font-size: 0.6em;">
+                                   My daughter Ella, a graduating senior and Senior Company member, ended her competition season on a perfect note with her best friend Lauren at her side.  The girls took one last picture together on stage and looking back, it really depicts the strong friendships that my daughter has gained through BG.  It was great that Ella and Lauren ended their time at BG together.  They still talk about how their friendship began with they were in Petite Ensemble.  Thank you to Marissa and her team for all they have done for Ella.  When she was 9, I remember asking her what she loves the most about dance and she said it made her feel free and that she can be herself.  I will always remember that!
+                               </p>
+                               <div class="d-flex align-items-center">
+                                   {{--                                   <ion-icon name="person-circle-outline" style="font-size: 2em; color: #d33;"></ion-icon>--}}
+                                   &nbsp; <span class="lead d-none d-md-block" style="font-size: 1.1em;">-Teresa Agabob</span>
+                                   <span class="lead d-block d-md-none" style="font-size: 0.6em;">-Teresa Agabob</span>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="carousel-item">
                            <div class="px-4 py-3">
                                <p class="fw-bold d-none d-md-block mb-0" style="font-size: 1.2em;">
                                    Breaking Ground Dance Center has boys in different classes and age groups. What I like about this school is that the schedule and expectations are shared with everyone early and the bill includes photography and costumes.  The kids quickly welcome new comers. No drama or unnecessary competitiveness.  We tried a class and stayed. Additionally, they will have an expanded, renovated, space for all to enjoy including a homework/collaborative area <ion-icon name="heart"></ion-icon>
