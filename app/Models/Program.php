ca88fecf2155a9779093a66ee272dba30b18e0d8
denliedesign <customdenlie@gmail.com>
2022-01-04 09:27:36 -0600

    sluggable on 2

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Cviebrock\EloquentSluggable\Sluggable;
+//use Cviebrock\EloquentSluggable\Sluggable;
 
 class Program extends Model
 {
