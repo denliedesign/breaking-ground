76b2c83cf8c75679cccfb95950c97cf7304c9ff2
denliedesign <customdenlie@gmail.com>
2024-10-08 10:44:40 -0500

    enrollio form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,46 @@
         </button>
     </div>
 
+    <div>
+        <iframe
+
+            src="https://link.enrollio.ai/widget/form/aOW7QU0kxGbk9sCv0sJN"
+
+            style="width:100%;height:100%;border:none;border-radius:4px"
+
+            id="inline-aOW7QU0kxGbk9sCv0sJN"
+
+            data-layout="{'id':'INLINE'}"
+
+            data-trigger-type="alwaysShow"
+
+            data-trigger-value=""
+
+            data-activation-type="alwaysActivated"
+
+            data-activation-value=""
+
+            data-deactivation-type="neverDeactivate"
+
+            data-deactivation-value=""
+
+            data-form-name="Website Lead Capture Form"
+
+            data-height="628"
+
+            data-layout-iframe-id="inline-aOW7QU0kxGbk9sCv0sJN"
+
+            data-form-id="aOW7QU0kxGbk9sCv0sJN"
+
+            title="Website Lead Capture Form"
+
+        >
+
+        </iframe>
+
+        <script src="https://link.enrollio.ai/js/form_embed.js"></script>
+    </div>
+
     <div class="mx-3">
         <!-- WELCOME COMBO CONTROL -->
         @can('update', \App\Combo::class)
