7ba3764b87f0a92f555720216a66eff7a687febe
denliedesign <customdenlie@gmail.com>
2024-11-20 10:28:01 -0600

    reg link change

--- resources/views/testimonials.blade.php
+++ resources/views/testimonials.blade.php
@@ -3,7 +3,7 @@
         <div id="next-steps-copy" class="row row-cols-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-3 d-flex justify-content-center bg-white pt-5 pb-3" style="font-size: 0.85em;">
             <div class="next-step mt-3 mt-md-0 shadow">
                 <p class="txt-red"><strong>Ready To Leap In?</strong></p>
-                <a href="https://app.akadadance.com/customer/login?schoolId=AK600070J&c=1" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family">&#10097; Enroll today!</div></a>
+                <a href="https://portal.akadadance.com/auth?schoolId=363" target="_blank" class="btn-opacity"><div class="shadow btn btn-lg btn-red btn-family">&#10097; Enroll today!</div></a>
             </div>
             {{--            <div class="next-step mt-3 mt-md-0 shadow">--}}
             {{--                <p class="txt-blue">Want To Give It A Try?</p>--}}
